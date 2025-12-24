f0a0caf914537514386b554e747eb3603c4de3a2
chaimagayes <115845786+chaimagayes@users.noreply.github.com>
2025-12-24 15:09:30 +0100

    Update App.js

--- Gestion_Budget-main/src/App.js
+++ Gestion_Budget-main/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/login";
+import Register from "./pages/Register";
 import Dashboard from "./Dashboard/dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
 export default App;
+
